3f19800932d36e6ed24cd4f42d5a76662749f63a
Syed Rafay <117012642+isyedrafay@users.noreply.github.com>
2024-07-05 14:48:38 +0500

    6th problem

---
+++ Karel the Robot/StoneMasonKarel.py
@@ -0,0 +1,28 @@
+from karel.stanfordkarel import *
+
+# ----------------------------
+# function which should make Karel 
+# move to the beeper, pick it up, and
+# return home.
+def main():
+    build_column()
+	
+    while front_is_clear():
+        for _ in range(4):
+            move()
+        build_column()
+
+def build_column():
+    turn_left()
+    while front_is_clear():
+        put_beeper()
+        move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    while front_is_clear():
+        move()
+    turn_left()
+
+if __name__ == '__main__':
+    main()
