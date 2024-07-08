92874e8554bdc10b22492e857df1fd988486ae81
Syed Rafay <117012642+isyedrafay@users.noreply.github.com>
2024-07-08 16:32:51 +0500

    Create HelloName.py

---
+++ Karel the Robot/Intro to Python/HelloName.py
@@ -0,0 +1,11 @@
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+    result = input("What is your name?")
+    print("Hello" + result)
+
+
+if __name__ == '__main__':
+    main()
